0449e64f457ffbc29d500bbd3f8e5bf58428d40d
Muhammad-Hussain-Ullah <160456309+Muhammad-Hussain-Ullah@users.noreply.github.com>
2024-04-14 18:43:37 +0500

    basic starter version of todo app

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+let Todos = []
+let i = 1;
+let condition = "true"
+while(condition)
+{
+    let questions = await inquirer.prompt(
+    [
+        {
+            name : "firstQuestion",
+            type : "input",
+            message : "What would you like to add in your Todos"
+        },
+        {
+            name : "secondQuestion",
+            type : "confirm",
+            default : "true",
+            message : "Would you like to add more in your Todos"
+        }
+    ]
+);
+Todos.push(questions.firstQuestion)
+console.log(Todos)
+condition = questions.secondQuestion
+}
